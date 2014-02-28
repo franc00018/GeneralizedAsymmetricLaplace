ae40f584277f2939a22c0796e269bae01c429f2e
François Pelletier <franc00018@gmail.com>
2014-02-27 23:07:02 -0500

    gradient moyenne variance GMM

---
+++ R/mean.variance.GMM.gradient.R
@@ -0,0 +1,25 @@
+# Gradient of the GMM moment conditions on mean and variance
+# 
+# Author: Francois Pelletier
+#
+# LGPL-3.0
+###############################################################################
+
+
+#' Gradient of the GMM moment conditions on mean and variance
+#'
+#' Derivative of the moment conditions according to the vector of parameters
+#' @param param GAL parameters
+#' @return gradient matrix
+#'
+#' @author François Pelletier
+mean.variance.GMM.gradient <- function(param,Data,type="mu")
+{
+	if(type=="mu")
+	{
+		matrix(c(-1,0,-param[4],-param[3],
+						-2*(mean(Data)-param[1]-param[3]*param[4]),-2*param[2]*param[4],2*param[4]*(mean(Data)-param[1]-param[3]*param[4])+2*param[3]*param[4],-2*param[3]*(mean(Data)-param[1]-param[3]*param[4])-param[2]^2-param[3]^2),
+				nrow=4,ncol=2)
+	}
+}
+
